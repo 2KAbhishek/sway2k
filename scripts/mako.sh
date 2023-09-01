9c463ea261c7c01cb014c24e9926532656be5908
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-02 02:27:08 +0530

    feat: move mako and foot configs

--- scripts/mako.sh
+++ scripts/mako.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # wrapper script for mako
 
-USER_CONFIG_PATH="${HOME}/.config/mako/config"
+USER_CONFIG_PATH="${HOME}/.config/sway/mako.ini"
 
 if [ -f "$USER_CONFIG_PATH" ]; then
     USER_CONFIG=$USER_CONFIG_PATH
--- scripts/waybar.sh
+++ scripts/waybar.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 # wrapper script for waybar with args, see https://github.com/swaywm/sway/issues/5724
 
-USER_CONFIG_PATH=$HOME/.config/sway/waybar/config.jsonc
-USER_STYLE_PATH=$HOME/.config/sway/waybar/style.css
+USER_CONFIG_PATH="${HOME}/.config/sway/waybar/config.jsonc"
+USER_STYLE_PATH="${HOME}/.config/sway/waybar/style.css"
 
 if [ -f "$USER_CONFIG_PATH" ]; then
     USER_CONFIG=$USER_CONFIG_PATH

815f1bd602cf538d960cd642e3b15e37340d6114
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-12-22 08:46:15 +0530

    feat: generate background in user dir

--- tools/scripts/generate-bg.sh
+++ tools/scripts/generate-bg.sh
@@ -6,4 +6,4 @@ export ROOT=$2
 export BACKGROUND=$3
 
 # shellcheck disable=SC2002
-cat /usr/share/sway/templates/manjarosway-scalable.svg | envsubst > "$HOME/.config/sway/generated_background.svg"
+cat "$HOME/.config/sway/tools/templates/manjarosway-scalable.svg" | envsubst >"$HOME/.config/sway/user/generated_background.svg"

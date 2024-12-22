f11edf21a4949e1bb80f04358f9be2b2aaf26059
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-12-22 09:02:51 +0530

    feat: update templates path

--- tools/scripts/mako.sh
+++ tools/scripts/mako.sh
@@ -6,4 +6,4 @@ if [ -f "$USER_CONFIG_PATH" ]; then
     USER_CONFIG=$USER_CONFIG_PATH
 fi
 
-mako -c "${USER_CONFIG:-"/usr/share/sway/templates/mako"}" "$@"
+mako -c "${USER_CONFIG:-"$HOME/.config/sway/tools/templates/mako"}" "$@"
--- tools/scripts/waybar.sh
+++ tools/scripts/waybar.sh
@@ -14,4 +14,4 @@ if [ -f "$USER_STYLE_PATH" ]; then
     USER_STYLE=$USER_STYLE_PATH
 fi
 
-waybar -c "${USER_CONFIG:-"/usr/share/sway/templates/waybar/config.jsonc"}" -s "${USER_STYLE:-"/usr/share/sway/templates/waybar/style.css"}" > $(mktemp -t XXXX.waybar.log)
+waybar -c "${USER_CONFIG:-"$HOME/.config/sway/tools/templates/waybar/config.jsonc"}" -s "${USER_STYLE:-"$HOME/.config/sway/tools/templates/waybar/style.css"}" > $(mktemp -t XXXX.waybar.log)

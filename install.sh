3eb080a4627c9437d9a3173f4d8faa4d944f2da7
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-04-08 09:25:37 +0530

    fix: install script path

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ mkdir -p ~/Videos/Recordings/
 ln -sfnv "$PWD"/waybar/ ~/.config/waybar
 ln -sfnv "$PWD"/foot/ ~/.config/foot
 ln -sfnv "$PWD"/swappy/ ~/.config/swappy
-ln -sfnv "$PWD"/sway/ ~/.config/sway
+ln -sfnv "$PWD"/../sway2k/ ~/.config/sway
